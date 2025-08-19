fabf48fd32da5a6030b9f51d3e7ee5fd556e3ac3
Esheshwari-K <esheshwarikumarbca24@rvu.edu.in>
2025-08-19 09:45:14 +0530

    Delete First.java

--- First.java
+++
@@ -1,9 +0,0 @@
-class First{
-    public static void main (String args[]) {
-        //println will send the text to next line coz it works as \n no needa insert \n
-System.out.println("Hello World"); //class=system, println=function 
-System.out.print("Hello World\n");//instead of println we can use \n
-System.out.print("Hello World");
-System.out.println("Hello World\n From \nApni Kaksha");//space before from is bcz of space in print statement after \n
-    }
-}
